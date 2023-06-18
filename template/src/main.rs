1aea273a33aa6b54dd2416be4c1ad3d3f74ae8ce
nukopy <nukopy@gmail.com>
2023-06-18 16:01:22 +0900

    feat: add template directory

---
+++ template/src/main.rs
@@ -0,0 +1,13 @@
+mod sec01;
+mod sec02;
+mod sec03;
+mod sec04;
+mod sec05;
+
+fn main() {
+    sec01::main();
+    // sec02::main();
+    // sec03::main();
+    // sec04::main();
+    // sec05::main();
+}
